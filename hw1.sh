f4c100f363b6b88425819e3fbafd2f747a606c7f
Paul Wessel <pwessel@hawaii.edu>
2019-07-09 15:58:13 -1000

    Make a hw1 directory with test scripts (#8)
    This allows us to supply more than one test scripts.  test1 checks for basic GMT functionality, test2 checks for ffmpeg.

--- hw1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Simple script to run a few GMT commands to make sure your installation works
-gmt begin hw1
-	gmt grdimage @earth_relief_20m -R0/40/0/40 -JM15c -I+d -U
-	gmt coast -W1p -B
-gmt end show
